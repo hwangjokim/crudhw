876a28ace5b9b191463fddaba8cc812c2132259e
hwangjo-v6x <hwangjo.kim@voyagerx.com>
2024-05-22 02:20:36 +0900

    Initial commit

---
+++ src/main/java/com/example/crudhw/CrudhwApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudhwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudhwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudhw/CrudhwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudhwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
